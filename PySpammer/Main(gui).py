175ed8fff0ec43a4ac5176dd226c93076b96582e
ps726 <91644659+ps726@users.noreply.github.com>
2021-11-05 02:06:39 +1100

    its fucking 2 in the morning i am too tired to write
    a real commit

--- PySpammer/Main(gui).py
+++ PySpammer/Main(gui).py
@@ -1,42 +1,51 @@
 import tkinter as tk
 from tkinter import *
 import tkinter.font as tkFont
-import tkinter
 import PySpammer
+
+bg = "#5A4955"
+Buttons = "#FFA500"
+
+#5A4955
+#808080
+#808080
+#454545
 
 window = tk.Tk()
 
 window.geometry("500x600")
 
+window.title('PySpammer')
+
 fontStyle = tkFont.Font(size=30)
 
-label1 = tk.Label(text="ᑭYᔕᑭᗩᗰᗰEᖇ", font=fontStyle)
+window.configure(background=bg)
+
+label1 = tk.Label(text="ᑭYᔕᑭᗩᗰᗰEᖇ V2", font=fontStyle, bg=bg)
 label1.pack()
 
-space3 = tk.Label(text=" ")
-space4 = tk.Label(text=" ")
+space3 = tk.Label(text=" ", bg=bg)
 space3.pack()
-space4.pack()
 
-B = tk.Button(text ="paste", width=50, height=2, compound="c", command = PySpammer.paste)
+B = tk.Button(text ="paste", width=50, height=2, compound="c", bg="#FFA500", borderwidth="2.5", command = PySpammer.paste)
 B.pack()
 
-canvas1 = tk.Canvas(window, width = 400, height = 200)
-canvas1.pack()
+e = Entry(window, width=50)
+e.pack()
 
-entry1 = tk.Entry (window) 
-canvas1.create_window(200, 140, window=entry1)
+def test():
+    PySpammer.say(e.get())
 
-B2 = tk.Button(text ="start spamming", width=50, height=2, compound="c", command = PySpammer.paste)
+B2 = tk.Button(text ="start spamming", width=50, height=2, compound="c", bg=Buttons, command = test)
 B2.pack()
 
-space1 = tk.Label(text=" ")
-space2 = tk.Label(text=" ")
+space1 = tk.Label(text=" ", bg=bg)
+
+space2 = tk.Label(text=" ", bg=bg)
 space1.pack()
 space2.pack()
 
-B3 = tk.Button(text ="Hello", width=50, height=2, compound="c", command = PySpammer)
+B3 = tk.Button(text ="random int", width=50, height=2, compound="c", bg=Buttons, command = PySpammer.say_random)
 B3.pack()
 
 window.mainloop()
-
