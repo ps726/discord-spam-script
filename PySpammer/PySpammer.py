7d4904fe3eb3c0c4ddf692ef7816ecb5af6a32f9
ps726 <91644659+ps726@users.noreply.github.com>
2021-11-05 02:07:40 +1100

    its 2 in the morning why am i doing this

--- PySpammer/PySpammer.py
+++ PySpammer/PySpammer.py
@@ -1,6 +1,10 @@
 import time
 from subprocess import run
+from typing import Text
 from pynput.keyboard import *
+import random
+
+messager = (str(random.randint(1,100)))
 
 keyboard = Controller()
 
@@ -24,3 +28,12 @@ def paste():
             keyboard.release(Key.ctrl)
             keyboard.press(Key.enter)
             keyboard.release(Key.enter)
+
+def say_random():
+    time.sleep(5)
+    while running == True:
+        messager = (str(random.randint(1,1000)))
+        keyboard.type(messager)
+        time.sleep(1)
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
