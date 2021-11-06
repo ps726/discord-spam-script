f5863b320867acf224ae4c5b92c26e6d96d82575
ps726 <91644659+ps726@users.noreply.github.com>
2021-11-06 14:29:22 +1100

    added B E E  M O V I E
    and someother stuff idk lmao

--- PySpammer/Main(gui).py
+++ PySpammer/Main(gui).py
@@ -3,7 +3,7 @@ from tkinter import *
 import tkinter.font as tkFont
 import PySpammer
 
-bg = "#5A4955"
+bg = "#181818"
 Buttons = "#FFA500"
 
 #5A4955
@@ -27,25 +27,50 @@ label1.pack()
 space3 = tk.Label(text=" ", bg=bg)
 space3.pack()
 
-B = tk.Button(text ="paste", width=50, height=2, compound="c", bg="#FFA500", borderwidth="2.5", command = PySpammer.paste)
-B.pack()
-
-e = Entry(window, width=50)
+e = Entry(window, width=50, borderwidth=5)
 e.pack()
 
-def test():
+space8 = tk.Label(text=" ", bg=bg)
+space9 = tk.Label(text=" ", bg=bg)
+space8.pack()
+space9.pack()
+
+def get_user_input():
     PySpammer.say(e.get())
 
-B2 = tk.Button(text ="start spamming", width=50, height=2, compound="c", bg=Buttons, command = test)
+B2 = tk.Button(text ="start spamming", width=50, height=2, compound="c", bg=Buttons, command = get_user_input)
 B2.pack()
 
 space1 = tk.Label(text=" ", bg=bg)
+space2 = tk.Label(text=" ", bg=bg)
+space1.pack()
+space2.pack()
 
+B = tk.Button(text ="paste", width=50, height=2, compound="c", bg="#FFA500", borderwidth="2.5", command = PySpammer.paste)
+B.pack()
+
+space1 = tk.Label(text=" ", bg=bg)
 space2 = tk.Label(text=" ", bg=bg)
 space1.pack()
 space2.pack()
 
 B3 = tk.Button(text ="random int", width=50, height=2, compound="c", bg=Buttons, command = PySpammer.say_random)
+B3.pack()
+
+space6 = tk.Label(text=" ", bg=bg)
+space7 = tk.Label(text=" ", bg=bg)
+space6.pack()
+space7.pack()
+
+B3 = tk.Button(text ="bee movie ;)", width=50, height=2, compound="c", bg=Buttons, command = PySpammer.read_file)
+B3.pack()
+
+space10 = tk.Label(text=" ", bg=bg)
+space11 = tk.Label(text=" ", bg=bg)
+space10.pack()
+space11.pack()
+
+B3 = tk.Button(text ="exit", width=25, height=2, compound="c", bg="#CC0000", command = exit)
 B3.pack()
 
 window.mainloop()
