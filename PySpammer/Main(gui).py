68d542bcd504b9a3dfcd3223e800bd34683f559f
ps726 <91644659+ps726@users.noreply.github.com>
2022-01-25 21:48:03 +1100

    added mee6 into gui
    added mee6 bot farm button

--- PySpammer/Main(gui).py
+++ PySpammer/Main(gui).py
@@ -13,27 +13,30 @@ Buttons = "#FFA500"
 
 window = tk.Tk()
 
-window.geometry("500x600")
+window.geometry("500x650")
 
 window.title('PySpammer')
+
+window.iconbitmap("OtherThings/PythonIcon.ico")
 
 fontStyle = tkFont.Font(size=30)
 
 window.configure(background=bg)
 
-label1 = tk.Label(text="ᑭYᔕᑭᗩᗰᗰEᖇ V2", font=fontStyle, bg=bg)
+label1 = tk.Label(text="ᑭYᔕᑭᗩᗰᗰEᖇ V2", font=fontStyle, bg=bg, foreground="white")
 label1.pack()
 
 space3 = tk.Label(text=" ", bg=bg)
+space2 = tk.Label(text=" ", bg=bg)
+space2.pack()
 space3.pack()
+
 
 e = Entry(window, width=50, borderwidth=5)
 e.pack()
 
 space8 = tk.Label(text=" ", bg=bg)
-space9 = tk.Label(text=" ", bg=bg)
 space8.pack()
-space9.pack()
 
 def get_user_input():
     PySpammer.say(e.get())
@@ -42,35 +45,45 @@ B2 = tk.Button(text ="start spamming", width=50, height=2, compound="c", bg=Butt
 B2.pack()
 
 space1 = tk.Label(text=" ", bg=bg)
-space2 = tk.Label(text=" ", bg=bg)
 space1.pack()
-space2.pack()
 
 B = tk.Button(text ="paste", width=50, height=2, compound="c", bg="#FFA500", borderwidth="2.5", command = PySpammer.paste)
 B.pack()
 
-space1 = tk.Label(text=" ", bg=bg)
 space2 = tk.Label(text=" ", bg=bg)
-space1.pack()
 space2.pack()
 
 B3 = tk.Button(text ="random int", width=50, height=2, compound="c", bg=Buttons, command = PySpammer.say_random)
 B3.pack()
 
 space6 = tk.Label(text=" ", bg=bg)
-space7 = tk.Label(text=" ", bg=bg)
 space6.pack()
-space7.pack()
 
-B3 = tk.Button(text ="bee movie ;)", width=50, height=2, compound="c", bg=Buttons, command = PySpammer.read_file)
-B3.pack()
+B4 = tk.Button(text ="bee movie ;)", width=50, height=2, compound="c", bg=Buttons, command = PySpammer.read_file)
+B4.pack()
 
 space10 = tk.Label(text=" ", bg=bg)
-space11 = tk.Label(text=" ", bg=bg)
 space10.pack()
-space11.pack()
 
-B3 = tk.Button(text ="exit", width=25, height=2, compound="c", bg="#CC0000", command = exit)
-B3.pack()
+B4 = tk.Button(text ="custom file", width=50, height=2, compound="c", bg=Buttons, command = PySpammer.read_custom_file)
+B4.pack()
+
+space10 = tk.Label(text=" ", bg=bg)
+space10.pack()
+
+B45 = tk.Button(text="AutoClicker",width=50, height=2, compound="c", bg=Buttons, command = PySpammer.read_custom_file )
+B45.pack()
+
+space2 = tk.Label(text=" ", bg=bg)
+space2.pack()
+
+B45 = tk.Button(text="mee6 bot farm",width=50, height=2, compound="c", bg=Buttons, command = PySpammer.mee6_bot_farm )
+B45.pack()
+
+space2 = tk.Label(text=" ", bg=bg)
+space2.pack()
+
+b5 = tk.Button(text ="exit", width=25, height=2, compound="c", bg="#CC0000", command = exit)
+b5.pack()
 
 window.mainloop()
